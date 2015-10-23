3bf20d406be26417655fe8a352f12c5bef91ce9f
Humakt83 <urlact@yahoo.com>
2015-10-23 11:04:00 +0300

    Displays answers for text report

--- app/src/main/java/fi/ukkosnetti/symprap/TextReportActivity.java
+++ app/src/main/java/fi/ukkosnetti/symprap/TextReportActivity.java
@@ -6,7 +6,7 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.StackView;
 import android.widget.TextView;
 
@@ -34,7 +34,22 @@ public class TextReportActivity extends ReportActivity {
     @Override
     protected void renderAnswers(final List<AnswerGet> answers) {
         Collections.sort(answers);
-        textAnswerView.setAdapter(new ArrayAdapter<AnswerGet>(getApplicationContext(), R.layout.text_answer_report_item) {
+        textAnswerView.setAdapter(new BaseAdapter() {
+
+            @Override
+            public int getCount() {
+                return answers.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return answers.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
