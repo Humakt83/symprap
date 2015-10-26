7c8ce024207b817db93ed9b12c76550935a36442
Humakt83 <urlact@yahoo.com>
2015-10-26 16:15:44 +0200

    Boolean answers report with pie diagram

--- app/src/main/java/fi/ukkosnetti/symprap/BooleanReportActivity.java
+++ app/src/main/java/fi/ukkosnetti/symprap/BooleanReportActivity.java
@@ -2,13 +2,25 @@ package fi.ukkosnetti.symprap;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import fi.ukkosnetti.symprap.dto.AnswerGet;
+import fi.ukkosnetti.symprap.reports.BooleanPieChart;
+import fi.ukkosnetti.symprap.util.Constants;
 
 public class BooleanReportActivity extends ReportActivity {
+
+    protected @Bind(R.id.booleanCanvasLayout) FrameLayout drawingLayout;
+    protected @Bind(R.id.booleanReportDate) TextView dateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,24 @@ public class BooleanReportActivity extends ReportActivity {
 
     @Override
     protected void renderAnswers(List<AnswerGet> answers) {
+        drawingLayout.addView(createPieChart(answers));
+        Collections.sort(answers);
+        dateView.setText(String.format("%s - %s", formatAnswerCreationDate(answers.get(answers.size() - 1)),
+            formatAnswerCreationDate(answers.get(0))));
+    }
 
+    private String formatAnswerCreationDate(AnswerGet answer) {
+        return Constants.DATETIME_FORMATTER.format(new Date(answer.created));
+    }
+
+    @NonNull
+    private View createPieChart(List<AnswerGet> answers) {
+        Long noes = 0l;
+        Long yesses = 0l;
+        for (AnswerGet answer : answers) {
+            if (Boolean.parseBoolean(answer.answer)) yesses++;
+            else noes++;
+        }
+        return new BooleanPieChart(this, yesses, noes);
     }
 }
---
+++ app/src/main/java/fi/ukkosnetti/symprap/reports/BooleanPieChart.java
@@ -0,0 +1,28 @@
+package fi.ukkosnetti.symprap.reports;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.view.View;
+
+public class BooleanPieChart extends View {
+
+    private Float noPercentage;
+
+    public BooleanPieChart(Context context, Long yesses, Long noes) {
+        super(context);
+        noPercentage = (Float.valueOf(noes)/Float.valueOf(yesses + noes)) * 100;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        RectF dimensions = new RectF(20, 20, 400, 400);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.GREEN);
+        canvas.drawArc(dimensions, 0, 360, true, paint);
+        paint.setColor(Color.RED);
+        canvas.drawArc(dimensions, 0, noPercentage * 3.6f, true, paint);
+    }
+}
